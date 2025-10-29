7651e96710c1c1bb97d73227eb14941aa764990b
Pawan Alur <alur.pawan.pa@gmail.com>
2025-10-29 13:43:30 +0530

    Basic router setup for blank Login and Signup pages.

---
+++ src/User/pages/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <>
+      <h1>Login Page</h1>
+      <p>This is under construction</p>
+    </>
+  );
+}
+
+export default Login;
---
+++ src/User/pages/Signup.jsx
@@ -0,0 +1,10 @@
+function Signup() {
+  return (
+    <>
+      <h1>Signup Page</h1>
+      <p>This is under construction</p>
+    </>
+  );
+}
+
+export default Signup;
